23b570b93a999ed5c915b436dfde9026061a1ed5
André Mendonça <andremendonca.033@gmail.com>
2022-02-28 01:51:03 -0300

    Script Improvements

--- script.js
+++ script.js
@@ -3,35 +3,39 @@ const addBtn = document.querySelector('[data-todo="add"]');
 const list = document.querySelector('[data-todo="list"]');
 const listItem = document.querySelector('[data-todo="listItem"]');
 
-function handleAddItem() {
-   if (field.value) {
-      const newItem = listItem.cloneNode(true);
-      newItem.children[2].innerText = field.value;
+if (field) {
+   function handleAddItem() {
+      if (field.value) {
+         const newItem = listItem.cloneNode(true);
+         newItem.children[2].innerText = field.value;
 
-      listItem.insertAdjacentElement('afterend', newItem);
-      field.value = '';
+         listItem.insertAdjacentElement('afterend', newItem);
+         field.value = '';
 
-      const deleteBtn = newItem.querySelector('[data-todo="delete"]');
-      const checkBtn = newItem.querySelector('[data-todo="checkbox"]');
+         const deleteBtn = newItem.querySelector('[data-todo="delete"]');
+         const checkBtn = newItem.querySelector('[data-todo="checkbox"]');
 
-      function handleCheckItem() {
-         this.parentElement.classList.toggle('checked');
-         if (!this.parentElement.classList.contains('checked')) {
-            listItem.insertAdjacentElement('afterend', this.parentElement);
-         } else {
-            list.appendChild(this.parentElement);
+         function handleCheckItem() {
+            this.parentElement.classList.toggle('checked');
+            if (!this.parentElement.classList.contains('checked')) {
+               listItem.insertAdjacentElement('afterend', this.parentElement);
+            } else {
+               list.appendChild(this.parentElement);
+            }
          }
-      }
-      function handleDeleteItem() {
-         this.parentElement.remove();
+         function handleDeleteItem() {
+            this.parentElement.remove();
 
-         checkBtn.removeEventListener('click', handleCheckItem);
-         deleteBtn.removeEventListener('click', handleDeleteItem);
-      }
+            checkBtn.removeEventListener('click', handleCheckItem);
+            deleteBtn.removeEventListener('click', handleDeleteItem);
+         }
 
-      checkBtn.addEventListener('click', handleCheckItem);
-      deleteBtn.addEventListener('click', handleDeleteItem);
+         checkBtn.addEventListener('click', handleCheckItem);
+         deleteBtn.addEventListener('click', handleDeleteItem);
+      }
    }
-}
 
-addBtn.addEventListener('click', handleAddItem);
+   addBtn.addEventListener('click', handleAddItem);
+} else {
+   window.location.reload();
+}
